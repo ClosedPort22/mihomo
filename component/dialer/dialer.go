d7f9ed8f6340020ceebb28dc31db0780453d5b7b
ClosedPort22 <44864697+ClosedPort22@users.noreply.github.com>
2026-05-28 14:07:13 +0000

    test

--- component/dialer/dialer.go
+++ component/dialer/dialer.go
@@ -142,7 +142,10 @@ func dialContext(ctx context.Context, network string, destination netip.Addr, po
 
 	if DefaultSocketHook != nil { // ignore interfaceName, routingMark and tfo when DefaultSocketHook not null (in CMFA)
 		socketHookToToDialer(dialer)
-	} else {
+		return dialer.DialContext(ctx, network, address)
+	}
+
+	if opt.interfaceName != "system" {
 		if opt.interfaceName == "" {
 			opt.interfaceName = DefaultInterface.Load()
 		}
@@ -160,15 +163,16 @@ func dialContext(ctx context.Context, network string, destination netip.Addr, po
 				return nil, err
 			}
 		}
-		if opt.routingMark == 0 {
-			opt.routingMark = int(DefaultRoutingMark.Load())
-		}
-		if opt.routingMark != 0 {
-			bindMarkToDialer(opt.routingMark, dialer, network, destination)
-		}
-		if opt.tfo && !DisableTFO {
-			return dialTFO(ctx, *dialer, network, address)
-		}
+	}
+	
+	if opt.routingMark == 0 {
+		opt.routingMark = int(DefaultRoutingMark.Load())
+	}
+	if opt.routingMark != 0 {
+		bindMarkToDialer(opt.routingMark, dialer, network, destination)
+	}
+	if opt.tfo && !DisableTFO {
+		return dialTFO(ctx, *dialer, network, address)
 	}
 
 	return dialer.DialContext(ctx, network, address)
